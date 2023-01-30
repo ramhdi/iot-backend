e3986e09a4b04e4d1fdf6fb555c0aab422b2972e
Rama Rahardi <rama.orion@gmail.com>
2023-01-30 14:02:25 +0700

    Error handling for DB

--- src/api/retriever.rs
+++ src/api/retriever.rs
@@ -1,6 +1,8 @@
 use crate::{connector::connector::MongoDB, model::device::DeviceData};
 use axum::{
     extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
     Json,
 };
 use mongodb::{
@@ -32,46 +34,75 @@ pub async fn get_dummy_data() -> Json<DeviceData> {
     return Json(dummy_data);
 }
 
-pub async fn get_data_by_id(
-    State(client): State<Client>,
-    oid: Path<String>,
-) -> Json<Option<DeviceData>> {
+pub async fn get_data_by_id(State(client): State<Client>, oid: Path<String>) -> impl IntoResponse {
     println!("get_data_by_id");
-    let id = oid.0;
+    let id_str = oid.0;
 
-    let col = MongoDB::init_collection(client)
-        .await
-        .unwrap()
-        .get_collection();
-
-    let find_result = col
-        .find_one(doc! {"_id": ObjectId::parse_str(id).unwrap()}, None)
-        .await;
-    let res = match find_result {
-        Ok(None) => None,
-        Ok(Some(data)) => Some(data),
-        Err(_) => None,
-    };
-    return Json(res);
+    let col_result = MongoDB::init_collection(client).await;
+    match col_result {
+        Ok(c) => {
+            // Success connecting to DB
+            let col = c.get_collection();
+            // Find document with corresponding id
+            let id = ObjectId::parse_str(id_str);
+            match id {
+                Ok(id) => {
+                    let filter = doc! {"_id": id};
+                    let find_result = col.find_one(filter, None).await;
+                    match find_result {
+                        Ok(None) => return (StatusCode::NOT_FOUND, Err("Not found")),
+                        Ok(Some(data)) => return (StatusCode::OK, Ok(Json(data))),
+                        Err(e) => {
+                            println!("{}", e);
+                            return (StatusCode::INTERNAL_SERVER_ERROR, Err("Error finding data"));
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("{}", e);
+                    return (StatusCode::BAD_REQUEST, Err("Invalid ID"));
+                }
+            }
+        }
+        Err(e) => {
+            // Failed connecting to DB
+            println!("{}", e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Err("Failed connecting to DB"),
+            );
+        }
+    }
 }
 
-pub async fn get_latest_data(State(client): State<Client>) -> Json<DeviceData> {
+pub async fn get_latest_data(State(client): State<Client>) -> impl IntoResponse {
     println!("get_latest_data");
-    let col = MongoDB::init_collection(client)
-        .await
-        .unwrap()
-        .get_collection();
+    let col_result = MongoDB::init_collection(client).await;
+    match col_result {
+        Ok(c) => {
+            // Success connecting to DB
+            let col = c.get_collection();
+            // Find document with latest timestamp
+            let find_options = FindOptions::builder()
+                .sort(doc! {"timestamp": -1})
+                .limit(1)
+                .build();
 
-    // Find document with latest timestamp
-    let find_options = FindOptions::builder()
-        .sort(doc! {"timestamp": -1})
-        .limit(1)
-        .build();
-    let find_result = col
-        .find(None, find_options)
-        .await
-        .unwrap()
-        .deserialize_current()
-        .unwrap();
-    return Json(find_result);
+            let find_result = col
+                .find(None, find_options)
+                .await
+                .unwrap()
+                .deserialize_current()
+                .unwrap();
+            return (StatusCode::OK, Ok(Json(find_result)));
+        }
+        Err(e) => {
+            // Failed connecting to DB
+            println!("{}", e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Err("Failed connecting to DB"),
+            );
+        }
+    }
 }
